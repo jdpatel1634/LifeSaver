41c4b31957d3a9e2801f3941a03ffd9a932506cb
Priyanshu Parmar <paparmar2004@gmail.com>
2026-07-05 16:10:07 -0600

    Removing description field from blood groups migration

--- database/migrations/create_blood_groups_table.php
+++ database/migrations/create_blood_groups_table.php
@@ -14,9 +14,6 @@ return new class extends Migration
         Schema::create('blood_groups', function (Blueprint $table) {
             $table->id();
             $table->string('group_name')->unique();
-            $table->string('code', 5)->unique()->nullable();
-            $table->text('description')->nullable();
-            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
