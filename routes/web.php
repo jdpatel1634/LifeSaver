d32ee2915d45457aa4caa267e9b0d3bf1455b531
Jaydeep <jaydeeppatel1634@gmail.com>
2026-07-04 21:32:11 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\BloodRequestController;
-use App\Http\Controllers\DonorRegistrationController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -17,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(HomeController::class)->group(function () {
-    Route::get('/', 'index')->name('home');
-    Route::post('/find-blood', 'handleSearch')->name('blood.handleSearch');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
