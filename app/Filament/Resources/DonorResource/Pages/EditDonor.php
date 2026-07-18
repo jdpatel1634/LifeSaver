1880223a0f332e28ce096247bffb6239a51314e3
Vaidehi Patel <vaidehipatel8490@gmail.com>
2026-07-18 13:36:02 -0600

    Add donor admin edit page

---
+++ app/Filament/Resources/DonorResource/Pages/EditDonor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\DonorResource\Pages;
+
+use App\Filament\Resources\DonorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDonor extends EditRecord
+{
+    protected static string $resource = DonorResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
