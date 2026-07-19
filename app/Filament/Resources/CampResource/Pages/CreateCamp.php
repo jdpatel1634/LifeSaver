2ab32f72741b05aec2e327ad97b8fe2acc0fedfe
Vaidehi Patel <vaidehipatel8490@gmail.com>
2026-07-19 09:32:51 -0600

    Define camp creation page

---
+++ app/Filament/Resources/CampResource/Pages/CreateCamp.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CampResource\Pages;
+
+use App\Filament\Resources\CampResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCamp extends CreateRecord
+{
+    protected static string $resource = CampResource::class;
+}
