21f18361930531d62bb110080510577cfd81be27
Jaydeep Patel <jaydeeppatel1634@gmail.com>
2026-07-23 10:18:26 -0600

    Configure blood unit editing page

---
+++ app/Filament/Resources/BloodUnitResource/Pages/EditBloodUnit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\BloodUnitResource\Pages;
+
+use App\Filament\Resources\BloodUnitResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBloodUnit extends EditRecord
+{
+    protected static string $resource = BloodUnitResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
+
