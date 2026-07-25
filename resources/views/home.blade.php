992b2bda2170f2ac2f48bc2b3b5439c075ff0e2f
Vaidehi <vaidehipatel8490@gmail.com>
2026-07-25 17:48:40 -0600

    Add Vite for CSS and JS asset management

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Life Saver - Blood Bank Management System</title>
