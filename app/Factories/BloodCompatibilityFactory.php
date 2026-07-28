81827caac4bc02279dd173f5099e4188ba94afab
Jaydeep <jaydeeppatel1634@gmail.com>
2026-07-28 08:42:07 -0600

    Update BloodCompatibilityFactory.php

--- app/Factories/BloodCompatibilityFactory.php
+++ app/Factories/BloodCompatibilityFactory.php
@@ -6,7 +6,9 @@ class BloodCompatibilityFactory
 {
     public static function compatibleDonorGroups(string $recipientBloodGroup): array
     {
-        return match (strtoupper($recipientBloodGroup)) {
+        $recipientBloodGroup = self::normalizeBloodGroup($recipientBloodGroup);
+
+        return match ($recipientBloodGroup) {
             'A+' => ['A+', 'A-', 'O+', 'O-'],
             'A-' => ['A-', 'O-'],
             'B+' => ['B+', 'B-', 'O+', 'O-'],
@@ -21,10 +23,33 @@ class BloodCompatibilityFactory
 
     public static function canDonateTo(string $donorBloodGroup, string $recipientBloodGroup): bool
     {
+        $donorBloodGroup = self::normalizeBloodGroup($donorBloodGroup);
+
         return in_array(
-            strtoupper($donorBloodGroup),
+            $donorBloodGroup,
             self::compatibleDonorGroups($recipientBloodGroup),
             true
         );
+    }
+
+    private static function normalizeBloodGroup(string $bloodGroup): string
+    {
+        $bloodGroup = strtoupper(trim($bloodGroup));
+
+        return match ($bloodGroup) {
+            'A POSITIVE', 'A_POSITIVE', 'APOSITIVE', 'A PLUS', 'A+' => 'A+',
+            'A NEGATIVE', 'A_NEGATIVE', 'ANEGATIVE', 'A MINUS', 'A-' => 'A-',
+
+            'B POSITIVE', 'B_POSITIVE', 'BPOSITIVE', 'B PLUS', 'B+' => 'B+',
+            'B NEGATIVE', 'B_NEGATIVE', 'BNEGATIVE', 'B MINUS', 'B-' => 'B-',
+
+            'AB POSITIVE', 'AB_POSITIVE', 'ABPOSITIVE', 'AB PLUS', 'AB+' => 'AB+',
+            'AB NEGATIVE', 'AB_NEGATIVE', 'ABNEGATIVE', 'AB MINUS', 'AB-' => 'AB-',
+
+            'O POSITIVE', 'O_POSITIVE', 'OPOSITIVE', 'O PLUS', 'O+' => 'O+',
+            'O NEGATIVE', 'O_NEGATIVE', 'ONEGATIVE', 'O MINUS', 'O-' => 'O-',
+
+            default => $bloodGroup,
+        };
     }
 }
